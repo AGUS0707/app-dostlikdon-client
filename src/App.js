2bfc4de44ef32112c2a895d17eb6170e64f045cc
AGUS0707diyevasa <xudayberdiyevasadbek95@gmail.com>
2021-06-10 19:40:59 +0500

    Login Started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Login from "./pages/Login";
 
-function App() {
-  return (
-    <div>
-
-    </div>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+              <Route path="/" exact component={Home}/>
+              <Route path="/login" exact component={Login}/>
+            </Switch>
+        </BrowserRouter>
+    );
+};
 
 export default App;
